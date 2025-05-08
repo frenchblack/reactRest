5eb4994152c15a6747d7d81e07243b96dacac1b5
frenchblack <qkfqkfdl387@naver.com>
2025-05-08 12:32:38 +0900

    * 작업 내용  - 헤더 클릭시 url로 이동  - 화면에서 메뉴 명 가져올 수 있도록 변경.

--- reactRest/src/main/java/com/spring/react/vo/MenuVO.java
+++ reactRest/src/main/java/com/spring/react/vo/MenuVO.java
@@ -6,10 +6,11 @@ import lombok.Setter;
 @Setter @Getter
 public class MenuVO {
 	public String menu_cd;
-	public String p_cd;
+	public String p_cd; 
 	public String menu_nm;
 	public int menu_lvl;
 	public int s_ord;
 	public boolean use_yn;
 	public String etc;
+	public String menu_url;
 }
